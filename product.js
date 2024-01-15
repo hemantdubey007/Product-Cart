1b13266d4ddeff327368799e9fa6276fd7ac1a9c
Hemant <dubeyhemantjpr@gmail.com>
2024-01-15 22:23:30 +0530

    Created Product Cart

---
+++ product.js
@@ -0,0 +1,8 @@
+const productList = [
+    {id: 1, name:   "Rasmalai"   , price:480},
+    {id: 2, name: "Brownies", price:190},
+    {id: 3, name: "Muffins", price:950},
+    {id: 4, name: "Custards"   , price:675},
+    {id: 5, name: "Gulab Jamun"   , price:240}
+    
+];
